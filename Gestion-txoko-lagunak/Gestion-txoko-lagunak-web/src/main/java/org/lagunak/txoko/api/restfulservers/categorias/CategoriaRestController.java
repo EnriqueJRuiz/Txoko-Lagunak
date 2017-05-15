12f649397d24ef56d12497a2cd04db33bfa15ef1
Enrique J. Ruiz <enriquejruizj@gmail.com>
2017-05-15 07:31:28 +0200

    a

--- Gestion-txoko-lagunak/Gestion-txoko-lagunak-web/src/main/java/org/lagunak/txoko/api/restfulservers/categorias/CategoriaRestController.java
+++ Gestion-txoko-lagunak/Gestion-txoko-lagunak-web/src/main/java/org/lagunak/txoko/api/restfulservers/categorias/CategoriaRestController.java
@@ -2,15 +2,21 @@ package org.lagunak.txoko.api.restfulservers.categorias;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.lagunak.txoko.persisence.Categoria;
 import org.lagunak.txoko.service.interfaces.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 
 @RestController
@@ -20,7 +26,8 @@ public class CategoriaRestController {
 	@Autowired
 	CategoriaService caS;
 	
-	@RequestMapping(method = RequestMethod.GET,produces =   MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(method = RequestMethod.GET,
+			produces = MediaType.APPLICATION_JSON_VALUE)
  	public ResponseEntity<List<Categoria>> getAll() {
  		List<Categoria> ampliaciones = caS.getAll();
  		ResponseEntity<List<Categoria>> response = null;
@@ -33,5 +40,74 @@ public class CategoriaRestController {
  
  		return response;
   	}
+	
+	@RequestMapping(value = "/{codigo}", 
+			method = RequestMethod.GET,
+			produces =   MediaType.APPLICATION_JSON_VALUE)
+  	public ResponseEntity<Categoria> getById(@PathVariable("codigo") int id) {
+		Categoria categoria = caS.getById(id);
+ 		ResponseEntity<Categoria> response = null;
+ 
+ 		if (categoria == null) {// 404
+ 			response = new ResponseEntity<Categoria>(HttpStatus.NOT_FOUND);
+ 		} else {// 200
+ 			response = new ResponseEntity<Categoria>(categoria, HttpStatus.OK);
+ 		}
+ 
+ 		return response;
+ 	}
+	@RequestMapping(value="/{codigo}", 
+			method = RequestMethod.DELETE, 
+			produces = { MediaType.APPLICATION_JSON_VALUE,
+					MediaType.APPLICATION_XML_VALUE })
+ 	public ResponseEntity<Categoria> deleteCategoria(@PathVariable("codigo") int id){
+		Categoria categoria = caS.getById(id);
+ 		ResponseEntity<Categoria> response = null;
+ 		if (categoria == null) {
+ 			response = new ResponseEntity<Categoria>(HttpStatus.NOT_FOUND);
+ 		} else {
+ 			caS.delete(id);
+ 			response = new ResponseEntity<Categoria>(categoria, HttpStatus.NO_CONTENT);
+ 		}
+		return response;
+ 	}
+	@RequestMapping(value = "/{codigo}", 
+			method = RequestMethod.PUT,
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Categoria> update(@PathVariable("codigo") int id,@RequestBody Categoria categoria){
+		Categoria cate = caS.getById(id);
+		ResponseEntity<Categoria> response = null;
+		if(cate == null){
+			response= new ResponseEntity<Categoria> (HttpStatus.NOT_FOUND);
+		}else{
+			cate = caS.update(categoria);
+			response = new ResponseEntity<Categoria>(cate, HttpStatus.ACCEPTED);
+		}
+		return response;
+	}
+	
+	@RequestMapping( method = RequestMethod.POST, 
+			consumes = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE }, 
+			produces = { MediaType.APPLICATION_JSON_VALUE,
+			MediaType.APPLICATION_XML_VALUE })
+	public ResponseEntity<Void> create(@Valid @RequestBody Categoria categoria, UriComponentsBuilder ucBuilder){
+		ResponseEntity<Void> response=null;
+		try{
+			Categoria aux = caS.create(categoria);
+			HttpHeaders headers = new HttpHeaders();
+			headers.setLocation(ucBuilder.path("/api/ampliaciones/{codigo}").buildAndExpand(aux.getCodigo()).toUri());
+			response = new ResponseEntity<Void>(headers,HttpStatus.CREATED);
+		}catch(Error e){
+			response = new ResponseEntity<Void>(HttpStatus.NOT_ACCEPTABLE);
+		}
+		
+		return response;
+	}
+	
+	
+	
+	
 }
  
